6c703734e66f67248e7aacf5206148a9d94ae599
WillGabrielc137 <willysmgabrielc137@gmail.com>
2025-01-06 11:14:39 -0300

    teste

---
+++ JAVA/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo! Bem-vindo ao Java!");
+
+        int a = 5;
+        int b = 10;
+        int soma = a + b;
+
+        System.out.println("A soma de " + a + " e " + b + " é: " + soma);
+    }
+}
